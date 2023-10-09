89eca7b09b047fcd6604ad741c336ca863d45d25
EdwardONeon <edwardoneon@outlook.com>
2023-10-09 13:06:21 +0800

    Access token refresh on frontend

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -24,14 +24,19 @@ export class AuthService {
 
   logout() {
     this.removeSession();
+    this.router.navigate(['/login']);
   }
 
   getAccessToken() {
-    return localStorage.getItem('x-access-item');
+    return localStorage.getItem('x-access-token');
   }
 
   getRefreshToken() {
     return localStorage.getItem('x-refresh-token');
+  }
+
+  getUserId() {
+    return localStorage.getItem('user-id');
   }
 
   setAccessToken(accessToken: string) {
@@ -42,18 +47,35 @@ export class AuthService {
     localStorage.setItem('x-refresh-token', refreshToken);
   }
 
+  setUserId(userId: string) {
+    localStorage.setItem('userId', userId);
+  }
+
   private setSession(userId:string, accessToken: string, refreshToken: string) {
     localStorage.setItem('user-id', userId);
-    localStorage.setItem('access-token', accessToken);
-    localStorage.setItem('refresh-token', refreshToken);
+    localStorage.setItem('x-access-token', accessToken);
+    localStorage.setItem('x-refresh-token', refreshToken);
   }
 
   private removeSession() {
     localStorage.removeItem('user-id');
-    localStorage.removeItem('access-token');
-    localStorage.removeItem('refresh-token');
+    localStorage.removeItem('x-access-token');
+    localStorage.removeItem('x-refresh-token');
   }
 
+  getNewAccessToken() {
+    return this.http.get(`${this.webService.ROOT_URL}/users/me/access-token`, {
+      headers: {
+        'x-refresh-token': this.getRefreshToken()!,
+        '_id': this.getUserId()!
+      },
+      observe: 'response'
+    }).pipe(
+      tap((res: HttpResponse<any>) => {
+        this.setAccessToken(res.headers.get('x-access-token')!);
+      })
+    )
+  }
 
 
 }
--- frontend/src/app/web-req.interceptor.ts
+++ frontend/src/app/web-req.interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, throwError } from 'rxjs';
+import { EMPTY, Observable, catchError, empty, switchMap, tap, throwError } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -10,6 +10,8 @@ export class WebReqInterceptor implements HttpInterceptor {
 
   constructor(private authService: AuthService) { }
 
+  refreshingAccessToken: boolean = false;
+
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // handle the request
@@ -18,9 +20,36 @@ export class WebReqInterceptor implements HttpInterceptor {
       //call next() and handle the response
       return next.handle(req).pipe(
         catchError((err: HttpErrorResponse) => {
+
+          if(err.status == 401 && !this.refreshingAccessToken) {
+            // error status is 401 meaning not authorized
+            return this.refreshAccessToken().pipe(
+              switchMap(() => {
+                req = this.addAuthHeader(req);
+                return next.handle(req);
+              }),
+              catchError((err: any) => {
+                console.log(err)
+                this.authService.logout();
+                return EMPTY;
+              })
+            )
+          }
+
           return throwError(()=>err);
         })
       )
+  }
+
+  refreshAccessToken() {
+    this.refreshingAccessToken = true;
+    // call method in the auth service to refresh the access token
+    return this.authService.getNewAccessToken().pipe(
+      tap(() => {
+        this.refreshingAccessToken = false;
+        console.log("Access token is refreshed!");
+      })
+    );
   }
 
   addAuthHeader(request: HttpRequest<any>) {
