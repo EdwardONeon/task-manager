2e94c5f25a9303d3594fc3991114c91a5c7c862a
EdwardONeon <edwardoneon@outlook.com>
2023-10-09 13:21:14 +0800

    Added signup page

--- frontend/src/app/web-request.service.ts
+++ frontend/src/app/web-request.service.ts
@@ -36,4 +36,12 @@ export class WebRequestService {
     });
   }
 
+  signup(email: string, password: string) {
+    return this.http.post(`${this.ROOT_URL}/users`, {
+      email, password
+    }, {
+      observe: 'response'
+    });
+  }
+
 }
